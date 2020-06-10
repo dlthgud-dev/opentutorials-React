6f3ca5a31e59f636eff61870bb1c8c0d9dd17e21
dlthgud-dev <dlthgud-@naver.com>
2020-06-10 06:14:48 +0000

    Component - props & state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TOC from './components/TOC';
+import Content from './components/Content';
+import Subject from './components/Subject';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            subject: { title: 'WEB', sub: 'world wide web!' },
+            contents: [
+                { id: 1, title: 'HTML', desc: 'HTML is for information' },
+                { id: 2, title: 'CSS', desc: 'CSS is for design' },
+                { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' }
+            ]
+        }
+    }
+    render() {
+        return (
+            <div className="App">
+              <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+                <TOC data={this.state.contents}></TOC>
+                <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+                <Content title="React" desc="For UI"></Content>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/components/TOC.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+// table of content (목차)
+class TOC extends Component {    
+    render() {
+        var lists = [];
+        var data = this.props.data;
+        var i = 0;
+        while(i < data.length) {
+            lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+            i = i + 1;
+        }
+        return (
+            <nav>
+                {lists}
+            </nav>
+        );
+    }
+}
+
+export default TOC;
