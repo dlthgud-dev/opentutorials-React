0e59f4f41d0eefb724ca87664e9a78a4c73aae35
dlthgud-dev <dlthgud-@naver.com>
2020-06-11 06:38:28 +0000

    Event

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            mode: 'welcome',
+            selected_content_id: 3,
             subject: { title: 'WEB', sub: 'world wide web!' },
+            welcome: { title: 'Welcome', desc: 'Hello, React!!' },
             contents: [
                 { id: 1, title: 'HTML', desc: 'HTML is for information' },
                 { id: 2, title: 'CSS', desc: 'CSS is for design' },
@@ -19,12 +22,37 @@ class App extends Component {
         }
     }
     render() {
+        console.log('App render');
+        var _title, _desc = null;
+        if(this.state.mode === 'welcome') {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if(this.state.mode === 'read') {
+            var i = 0;
+            while(i < this.state.contents.length) {
+                var data = this.state.contents[i];
+                if(data.id === this.state.selected_content_id) {
+                    _title = data.title;
+                    _desc = data.desc;
+                    break;
+                }
+                i = i + 1;
+            }
+        }
+        console.log('render', this);
         return (
             <div className="App">
-              <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
-                <TOC data={this.state.contents}></TOC>
-                <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
-                <Content title="React" desc="For UI"></Content>
+              <Subject title={this.state.subject.title} sub={this.state.subject.sub}
+                  onChangePage={function() {
+                        this.setState({ mode: 'welcome' });
+                    }.bind(this)}></Subject>
+                <TOC onChangePage={function(id) {
+                        this.setState({ 
+                            mode: 'read',
+                            selected_content_id: Number(id)
+                        });
+                    }.bind(this)} data={this.state.contents}></TOC>
+                <Content title={_title} desc={_desc}></Content>
             </div>
         )
     }
