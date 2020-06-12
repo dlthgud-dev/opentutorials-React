18cf303151be546df785896e7446403e611972b6
dlthgud-dev <dlthgud-@naver.com>
2020-06-12 12:38:28 +0000

    add Delete

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
         // 마지막 content id
         this.max_content_id = 3;
         this.state = {
-            mode: 'create',
+            mode: 'welcome',
             selected_content_id: 3,
             subject: { title: 'WEB', sub: 'world wide web!' },
             welcome: { title: 'Welcome', desc: 'Hello, React!!' },
@@ -98,9 +98,29 @@ class App extends Component {
                         });
                     }.bind(this)} data={this.state.contents}></TOC>
                 <Control onChangeMode={function(_mode) {
-                        this.setState({
-                            mode: _mode
-                        });
+                        if(_mode === 'delete') {
+                            if(window.confirm('really?')) {
+                                var _contents = Array.from(this.state.contents);
+                                var i = 0;
+                                while(i < _contents.length) {
+                                    if(_contents[i].id === this.state.selected_content_id) {
+                                        _contents.splice(i, 1);
+                                        break;
+                                    }
+                                    i = i + 1;
+                                }
+                                this.setState({
+                                    mode: 'welcome',
+                                    contents: _contents
+                                });
+                                alert('deleted');
+                            }
+                        } else {
+                            this.setState({
+                                mode: _mode
+                            });
+                        }
+                        
                     }.bind(this)}></Control>
                 {this.getContent()}
             </div>
